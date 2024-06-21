d7083dc8e61585483c15f79721f8c36ec19f0296
Connor Warme <connor.warme@gmail.com>
2024-06-21 17:12:19 -0600

    Tweak Prod style

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -9,40 +9,43 @@ type ProductProps = {
   url: string,
 }
 
-const Product = (product: ProductProps) => {
+const Product = ({id, model, brand, price, url}: ProductProps) => {
   const quantity = 2;
+
+  // old code
+    // <div className="d-flex flex-column justify-content-center align-items-end">
+    // <Card.Text className="ms-auto">
+    //   {formatCurrency(price)}
+    // </Card.Text>
+    // { quantity > 1 && (
+    //   <Card.Text>
+    //     <span className="mx-2">x{quantity}</span>
+    //     {formatCurrency(price * quantity)}
+    //   </Card.Text>
+    // )}
   return ( 
-    <Card>
-      <Card.Img variant="top" src={product.url} />
+    <Card className="h-100">
+      <Card.Img variant="top" src={url} height="300px" style={{ objectFit: "cover" }} />
       <Card.Body>
-        <div className="d-flex">
-          <div className="me-auto">
-            <Card.Title>
-              {product.model}
-            </Card.Title>
-            <Card.Text>
-              {product.brand}
-            </Card.Text>
-          </div>
-          <div className="d-flex flex-column justify-content-center align-items-end">
-            <Card.Text className="ms-auto">
-              {formatCurrency(product.price)}
-            </Card.Text>
-            { quantity > 1 && (
-              <Card.Text>
-                <span className="mx-2">x{quantity}</span>
-                {formatCurrency(product.price * quantity)}
-              </Card.Text>
-            )}
-          </div>
+        <div className="d-flex flex-column">
+          <Card.Title className="d-flex justify-content-between align-items-center">
+            <div className="d-flex flex-column">
+              <span className="fs-2">{model}</span>
+              <span className="text-muted">{brand}</span>
+            </div>
+            <div className="ms-2 text-muted">{formatCurrency(price)}</div>
+          </Card.Title>
         </div>
-        <div className="my-2">
+        <div className="mt-auto">
           { quantity > 0 && (
             <div className="d-flex justify-content-center align-items-center gap-2">
-              <Button variant="outline-primary" className="me-2" style={{width: "3rem", height: "3rem"}}>
+              <Button variant="outline-primary" className="" style={{width: "3rem", height: "3rem"}}>
                 -
               </Button>
-              <Card.Text className="m-0 text-lg">{quantity}<span className="mx-2">in cart</span></Card.Text>
+              <Card.Text className="d-flex justify-content-center align-items-center ms-2 mb-0">
+                <span className="fs-2">{quantity}</span>
+                <span className="mx-2">in cart</span>
+              </Card.Text>
               <Button variant="outline-primary" style={{width: "3rem", height: "3rem"}}>
                 +
               </Button>
